47e9c153a0e80e98230a287341372047f36bbf76
Siddharth052002 <92661511+Siddharth052002@users.noreply.github.com>
2024-04-05 04:56:55 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+let name = "Sid"
+let age = 3
+let isLoggedIn = false
+
+// number -> 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => unique (used in react)
+
+
+// object
+
+console.log(typeof "sid");
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
