744e6f839208ab0281aee48a9572f20e07eab3ed
Siddharth052002 <92661511+Siddharth052002@users.noreply.github.com>
2024-04-05 05:50:11 +0000

    Conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,60 +1,86 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score))
+// console.log(typeof score);
+// console.log(typeof(score))
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
-
-
-let score2 = null
-
-console.log(typeof score2);
-console.log(typeof(score2))
-
-let valueInNumber2 = Number(score2)
-console.log(typeof valueInNumber2)
-console.log(valueInNumber2)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 
-let score3 = undefined
+// let score2 = null
 
-console.log(typeof score3);
-console.log(typeof(score3))
+// console.log(typeof score2);
+// console.log(typeof(score2))
 
-let valueInNumber3 = Number(score3)
-console.log(typeof valueInNumber3)
-console.log(valueInNumber3)
-
-
-let score4 = true
-
-console.log(typeof score4);
-console.log(typeof(score4))
-
-let valueInNumber4 = Number(score4)
-console.log(typeof valueInNumber4)
-console.log(valueInNumber4)
+// let valueInNumber2 = Number(score2)
+// console.log(typeof valueInNumber2)
+// console.log(valueInNumber2)
 
 
-// "33" => 33
-// "33abc" => NaN
-// true => 1
-// false => 0
+// let score3 = undefined
+
+// console.log(typeof score3);
+// console.log(typeof(score3))
+
+// let valueInNumber3 = Number(score3)
+// console.log(typeof valueInNumber3)
+// console.log(valueInNumber3)
 
 
-let isLoggedIn = 1
+// let score4 = true
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(typeof score4);
+// console.log(typeof(score4))
 
-// 1 => true; 0 => false
-// "" => false
-// "sid" => true
+// let valueInNumber4 = Number(score4)
+// console.log(typeof valueInNumber4)
+// console.log(valueInNumber4)
 
-let someNumber = 33;
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1
+// // false => 0
+
+
+// let isLoggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// // 1 => true; 0 => false
+// // "" => false
+// // "sid" => true
+
+// let someNumber = 33;
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+
+// **************** Operations *************
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+console.log(true)
+console.log(+true) // 1
+console.log(+"") // 0 because "" => false and + => conversion
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
