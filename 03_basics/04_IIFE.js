5e8b4e2f47f820e767d368a02fb31a04efb1c1e2
Siddharth052002 <92661511+Siddharth052002@users.noreply.github.com>
2024-04-08 15:49:01 +0000

    IIFE in JS

--- 03_basics/04_IIFE.js
+++ 03_basics/04_IIFE.js
@@ -3,6 +3,42 @@
 // *** IMP Why we use IIFE??
 // global scope ke pollution se dikkt hoti hai kai bar to usko resolve krne ke lie use krte hai IIFE
 
+// IIFEs are commonly used to create private scope in JavaScript, allowing variables and functions to be encapsulated and inaccessible from outside the function.
+
 (function chai() {
+    // named IIFE
     console.log("DB Connected")
-})()
+})();
+
+// NOTE: (semicolon ; is imp if two or more IIFE are there in order to end one)
+
+((name) => {
+    // Simple IIFE
+    console.log(`DB connected again ${name}`)
+})("Sid")
+
+// var counter = (function() {
+//     var count = 0;
+ 
+//     return {
+//         increment: function() {
+//             count++;
+//         },
+//         decrement: function() {
+//             count--;
+//         },
+//         getCount: function() {
+//             return count;
+//         }
+//     };
+// })();
+ 
+// // Increment the counter
+// counter.increment();
+// counter.increment();
+// counter.increment();
+ 
+// console.log(counter.getCount()); // Output: 3
+ 
+// // Trying to access the private count variable directly
+// console.log(counter.count); // Output: undefined (cannot access private variable)
