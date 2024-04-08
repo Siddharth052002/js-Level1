9a38ebb20bb122d51e330af68ef5903e28c76fea
Siddharth052002 <92661511+Siddharth052002@users.noreply.github.com>
2024-04-08 20:49:34 +0000

    While loop in JS

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+// while
+
+let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`)
+//     index = index + 2
+// }
+
+let myArr = ['flash', 'ads', 'def']
+
+let arr = 0
+// while(arr < myArr.length) {
+//     console.log(`Value is ${myArr[arr]}`);
+//     arr++;
+// }
+
+
+// *** do while loop
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
